ff8b47e41db543889f0b5f2cf1b1aacbd8152e23
Hashir313 <hashirmasood313@gmail.com>
2023-06-14 23:01:00 -0700

    V1

---
+++ Form1.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace tier3Architecture
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtProgram = new System.Windows.Forms.TextBox();
+            this.txtSemester = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtRollNo = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnViewByID = new System.Windows.Forms.Button();
+            this.btnView = new System.Windows.Forms.Button();
+            this.dgvStudentInfo = new System.Windows.Forms.DataGridView();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvStudentInfo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(304, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(187, 36);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Student Form";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 267);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(151, 22);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Student Semester:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 194);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(146, 22);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Student Program:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Times New Roman", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(12, 128);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(125, 22);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Student Name:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Times New Roman", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(12, 70);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(182, 22);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Student Roll Number:";
+            // 
+            // txtProgram
+            // 
+            this.txtProgram.Location = new System.Drawing.Point(207, 194);
+            this.txtProgram.Name = "txtProgram";
+            this.txtProgram.Size = new System.Drawing.Size(144, 26);
+            this.txtProgram.TabIndex = 7;
+            // 
+            // txtSemester
+            // 
+            this.txtSemester.Location = new System.Drawing.Point(207, 265);
+            this.txtSemester.Name = "txtSemester";
+            this.txtSemester.Size = new System.Drawing.Size(144, 26);
+            this.txtSemester.TabIndex = 8;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(207, 128);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(144, 26);
+            this.txtName.TabIndex = 9;
+            // 
+            // txtRollNo
+            // 
+            this.txtRollNo.Location = new System.Drawing.Point(207, 70);
+            this.txtRollNo.Name = "txtRollNo";
+            this.txtRollNo.Size = new System.Drawing.Size(144, 26);
+            this.txtRollNo.TabIndex = 10;
+            this.txtRollNo.Text = "123";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(27, 388);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(101, 40);
+            this.btnAdd.TabIndex = 11;
+            this.btnAdd.Text = "ADD";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnViewByID
+            // 
+            this.btnViewByID.Location = new System.Drawing.Point(63, 585);
+            this.btnViewByID.Name = "btnViewByID";
+            this.btnViewByID.Size = new System.Drawing.Size(200, 40);
+            this.btnViewByID.TabIndex = 12;
+            this.btnViewByID.Text = "View By Roll Number";
+            this.btnViewByID.UseVisualStyleBackColor = true;
+            this.btnViewByID.Click += new System.EventHandler(this.btnViewByID_Click);
+            // 
+            // btnView
+            // 
+            this.btnView.Location = new System.Drawing.Point(191, 378);
+            this.btnView.Name = "btnView";
+            this.btnView.Size = new System.Drawing.Size(101, 40);
+            this.btnView.TabIndex = 13;
+            this.btnView.Text = "View";
+            this.btnView.UseVisualStyleBackColor = true;
+            this.btnView.Click += new System.EventHandler(this.btnView_Click);
+            // 
+            // dgvStudentInfo
+            // 
+            this.dgvStudentInfo.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvStudentInfo.Location = new System.Drawing.Point(367, 59);
+            this.dgvStudentInfo.Name = "dgvStudentInfo";
+            this.dgvStudentInfo.RowHeadersWidth = 62;
+            this.dgvStudentInfo.RowTemplate.Height = 28;
+            this.dgvStudentInfo.Size = new System.Drawing.Size(631, 611);
+            this.dgvStudentInfo.TabIndex = 14;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(191, 494);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(101, 40);
+            this.btnUpdate.TabIndex = 15;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(27, 494);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(101, 40);
+            this.btnDelete.TabIndex = 16;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1010, 700);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.dgvStudentInfo);
+            this.Controls.Add(this.btnView);
+            this.Controls.Add(this.btnViewByID);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtRollNo);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.txtSemester);
+            this.Controls.Add(this.txtProgram);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvStudentInfo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtProgram;
+        private System.Windows.Forms.TextBox txtSemester;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtRollNo;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnViewByID;
+        private System.Windows.Forms.Button btnView;
+        private System.Windows.Forms.DataGridView dgvStudentInfo;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,98 @@
+﻿using BuisnessLogicLayer;
+using PropsLogicLayer;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tier3Architecture
+{
+    public partial class Form1 : Form
+    {
+        int rollNo, semester;
+        String name, program , message;
+        StudentProps studentProps = new StudentProps();
+        StudentBLL studentBLL = new StudentBLL();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        void clearTextFields()
+        {
+            txtName.Clear();
+            txtProgram.Clear();
+            txtRollNo.Clear();
+            txtSemester.Clear();
+        }
+
+        private void btnViewByID_Click(object sender, EventArgs e)
+        {
+            rollNo = Convert.ToInt32(txtRollNo.Text);
+            studentProps.StudentRollNo = rollNo;
+            DataTable searchRollNo = new DataTable();
+            searchRollNo = studentBLL.searchStudentsByRollNo(studentProps);
+            dgvStudentInfo.DataSource = searchRollNo;
+            clearTextFields();
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            rollNo = Convert.ToInt32(txtRollNo.Text);
+            semester = Convert.ToInt32(txtSemester.Text);
+            name = txtName.Text;
+            program = txtProgram.Text;
+
+            studentProps.StudentRollNo = rollNo;
+            studentProps.StudentSemester = semester;
+            studentProps.StudentProgram = program;
+            studentProps.StudentName = name;
+            message = studentBLL.updateStudentBLL(studentProps);
+            MessageBox.Show(message);
+            clearTextFields();
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            rollNo = Convert.ToInt32(txtRollNo.Text);
+            studentProps.StudentRollNo = rollNo;
+            message = studentBLL.deleteStudentBLL(studentProps);
+            MessageBox.Show(message);
+            clearTextFields();
+        }
+
+        private void btnView_Click(object sender, EventArgs e)
+        {
+            DataTable searchAll = new DataTable();
+            searchAll = studentBLL.searchAllStudents();
+            dgvStudentInfo.DataSource = searchAll;
+            clearTextFields();
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            rollNo = Convert.ToInt32(txtRollNo.Text);
+            semester = Convert.ToInt32(txtSemester.Text);
+            name = txtName.Text;
+            program = txtProgram.Text;
+
+            studentProps.StudentRollNo = rollNo;
+            studentProps.StudentSemester = semester;
+            studentProps.StudentProgram = program;
+            studentProps.StudentName = name;
+            message = studentBLL.insertStudentBLL(studentProps);
+            MessageBox.Show(message);
+            clearTextFields();
+        }
+    }
+}
